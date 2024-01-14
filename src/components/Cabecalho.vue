7edb810cde026aa4af3c313a64358c6ade0b4a8b
Pedro Santos <pedrohenriqueph004@gmail.com>
2024-01-14 12:19:10 -0300

    Alterando a mensagem do header cado haja 0 tarefas pendentes

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -1,15 +1,17 @@
 <script setup>
-  const props = defineProps(['tarefasPendentes'])
+const props = defineProps(['tarefasPendentes'])
 </script>
 
 <template>
-
   <header class="p-5 mb-4 mt-4 bg-light rounded-3">
     <h1>Minhas tarefas</h1>
 
-    <p>
+    <p v-if="props.tarefasPendentes > 0">
       Você possui {{ props.tarefasPendentes }} tarefas pendentes
     </p>
-  </header>
+    <p v-else>
+      Você não possui tarefas pendentes
+    </p>
 
+  </header>
 </template>
